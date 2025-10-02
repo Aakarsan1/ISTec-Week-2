ee7463bfe4c6e169f4a5dac1fe79e73df02c6036
Aakarsan1 <N1291617@my.ntu.ac.uk>
2025-10-02 12:50:16 +0100

    Create randomnumgen.py

---
+++ randomnumgen.py
@@ -0,0 +1,14 @@
+import random
+
+words = "hello"
+
+for i in range(1,5):
+    guess = input("What word do you think the system would choose? ")
+    if words == guess:
+        print("yes")
+      
+    else:
+        print("No")
+
+    
+print(x)
